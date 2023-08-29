41129419f83dc7927b9f7ae19ecce0620372a961
yuezhong <yuezhongl@gmail.com>
2023-08-29 17:10:36 +0800

    REST-API Server
    REST API server written in Nodejs

---
+++ MEAN-Example/RESTAPI-Server/db-details.js
@@ -0,0 +1,13 @@
+/**
+ *  Configuration for accessing the db.
+ *  These can be changed here and injected 
+ *  into different parts of the app depending on when
+ *  a db connection needs to be created.
+ */
+module.exports =
+{
+    host: "localhost",
+    user: "web2user",
+    password: "web2user",
+    database: "web2app"
+};
